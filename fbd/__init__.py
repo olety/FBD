2ca1c6c59d518255356f6aae89f52e7cec62d4f6
Oleksii Kyrylchuk <olety7@gmail.com>
2017-05-29 21:12:10 +0200

    Fixing imports for pip
    Other/Miscellaneous:
    - Moved the gathering script to bin/fbd-gather
    - Updated setup.py
    - Updated imports in every module so that `pip3 install fbd` workd

--- fbd/__init__.py
+++ fbd/__init__.py
@@ -1,10 +1,4 @@
-from gatherer import Gatherer
-from storage import Storage
-<<<<<<< HEAD
-from visualizer import Visualizer
-
-from . import tools
-=======
-from tools import *
-from visualizer import Visualizer
->>>>>>> d8d82e0b7e75a4987996c3d3ef8972696968efc2
+from . import gatherer, storage, tools, visualizer
+from .gatherer import Gatherer
+from .storage import Storage
+from .visualizer import Visualizer
