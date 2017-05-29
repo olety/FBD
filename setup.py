d8d82e0b7e75a4987996c3d3ef8972696968efc2
Oleksii Kyrylchuk <olety7@gmail.com>
2017-05-29 17:37:41 +0200

    Added example config files and updated __init__

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
 setup(name='fbd',
-      version='0.1',
+      version='0.0.1',
       description='Facebook data gatherer and analyzer',
-      url='',
+      url='https://github.com/olety/FBD',
       author='Oleksii Kyrylchuk',
       author_email='olkyrylchuk@gmail.com',
       license='MIT',
       packages=['fbd'],
+      install_requires=['requests', 'gmplot', 'geocoder', 'alembic', 'bokeh', 'matplotlib',
+                        'tqdm', 'setuptools', 'SQLAlchemy', 'numpy', 'python_dateutil'],
+      include_package_data=True,
       zip_safe=False)
